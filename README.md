8eab39f045a7053df4cf9b8900c0ee0623bacb69
Luciano Souza <lucianoiw@hotmail.com>
2013-09-09 14:06:23 -0300

    comecando projeto

--- README.md
+++ README.md
@@ -2,3 +2,41 @@ jYmbed
 ======
 
 Embed videos from youtube by the url of the video
+
+
+jYmbed is a jQuery simple plugin for embedding a Youtube videos from a url of the video. It provides a full and flexible toolset for embedding Youtube videos.
+
+Documentation
+-------------
+
+### Reference
+
+You can read the documentation of the version 0.1 [here](http://sorgalla.com/jcarousel/docs/):
+
+  * [Installation](http://sorgalla.com/jcarousel/docs/reference/installation.html)
+  * [Configuration](http://sorgalla.com/jcarousel/docs/reference/configuration.html)
+  * [API](http://sorgalla.com/jcarousel/docs/reference/api.html)
+  * [Usage](http://sorgalla.com/jcarousel/docs/reference/usage.html)
+  * [Events](http://sorgalla.com/jcarousel/docs/reference/events.html)
+
+Download
+--------
+
+All ready-to-use files are located in the [`dist/`](dist/) directory.
+
+The [`jquery.jYbmed.js`](dist/jquery.jYmbed.js?raw=1) and
+[`jquery.jYmbed.min.js`](dist/jquery.jYmbed.min.js?raw=1) files contain
+the core and all plugins concenated together.
+
+Examples
+--------
+
+You can find some example implementations in the [`examples/`](examples/)
+directory.
+
+License
+-------
+
+Copyright (c) 2013 Luciano Souza.
+Released under the [MIT](LICENSE?raw=1) license.
+
---
+++ dist/jquery.jYmbed.js
@@ -0,0 +1,79 @@
+/*! jYmbed - v0.1-beta - 2013-09-09
+* http://ketshup.com.br/jymbed
+* Copyright (c) 2013 Luciano Souza; Licensed MIT */
+
+(function($){
+	$.fn.jYmbed = function() {
+		var $regex = /(\?v=|\&v=|\/\d\/|\/embed\/|\/v\/|\.be\/)([a-zA-Z0-9\-\_]+)/;
+		var $attr = {
+			width: 560,
+			height: 315,
+			frameborder: 0,
+			allowfullscreen: true
+		};
+
+		var $params = {
+			autoplay: {
+				default: false,
+				key: "autoplay",
+				value: {
+					"false": 0,
+					"true": 1
+				}
+			},
+			suggested: {
+				default: false,
+				key: "rel",
+				value: {
+					"false": 0,
+					"true": 1
+				}
+			}
+		};
+
+		return this.each(function(i, item) {
+			var $this = $(item),
+				$yid = 0;
+
+			if($this.is("[data-id]")){
+				$yid = $this.data("id");
+			}else{
+				var $yurl = $this.is("[data-url]") ? $this.data("url") : $this.text(),
+					$ycheck = $yurl.match($regex);
+
+				if($ycheck){
+					$yid = $ycheck[2];
+				}
+			}
+
+			if($yid){
+				var $src = "http://www.youtube.com/embed/" + $yid;
+
+				$attr["width"] = $this.is("[data-width]") ? $this.data("width") : $attr["width"];
+				$attr["height"] = $this.is("[data-height]") ? $this.data("height") : $attr["height"];
+
+				var $param = [], $count = 0;
+				$.each($params, function(param, object){
+					if($this.is("[data-"+ param +"]")){
+						$param[$count] = object.key + "=" + object.value[$this.data(param)];
+					}
+					$count++;
+				});
+
+				if($this.is("[data-start]")){
+					$param[$count] = "start=" + $this.data("start");
+					$count++;
+				}
+
+				if($param.length){
+					$src = $src + "?" + $param.join("&")
+				}
+
+				$attr["src"] = $src;
+
+				var $embed = $("<iframe/>").attr($attr);
+				$this.html($embed);
+			}
+		});
+	};
+})(jQuery);
